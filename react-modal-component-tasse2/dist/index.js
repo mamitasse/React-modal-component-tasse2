5784a2036a19abffc086ba30002f292839e0d9bb
mamitasse <117809555+mamitasse@users.noreply.github.com>
2024-07-20 11:00:20 +0200

    debeug2

--- react-modal-component-tasse2/dist/index.js
+++ react-modal-component-tasse2/dist/index.js
@@ -1,15 +1,9 @@
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-require("./index.css");
-var _App = _interopRequireDefault(require("./App"));
-var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _Modal = _interopRequireDefault(require("./components/Modal"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var root = _client["default"].createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-(0, _reportWebVitals["default"])();
+var _default = exports["default"] = _Modal["default"];
